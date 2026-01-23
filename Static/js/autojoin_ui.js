2947991d1ad5e5c1f205e1656ee09749ffb292a9
Asuka Langley <liwenyu2021@gmail.com>
2026-01-22 23:06:58 -0700

    Try and try to fix autojoin

--- Static/js/autojoin_ui.js
+++ Static/js/autojoin_ui.js
@@ -1,6 +1,5 @@
 (function() {
     const storageKey = 'autojoin_active';
-    const joinWindows = {};
 
     const sendBeacon = (url, payload) => {
         const body = JSON.stringify(payload || {});
@@ -25,37 +24,8 @@
         }
     };
 
-    const preopenJoinWindow = (serverKey) => {
-        let win = null;
-        let blocked = false;
-        try {
-            win = window.open('about:blank', '_blank');
-            if (!win) blocked = true;
-        } catch (e) {
-            blocked = true;
-        }
-        joinWindows[serverKey] = { win, blocked };
-        return joinWindows[serverKey];
-    };
-
-    const navigateJoinWindow = (serverKey, url) => {
-        const entry = joinWindows[serverKey];
-        if (!entry || entry.blocked) return false;
-        try {
-            const w = entry.win;
-            if (!w || w.closed) return false;
-            w.location.href = url;
-            return true;
-        } catch (e) {
-            return false;
-        }
-    };
-
     window.AutojoinUI = {
         storageKey,
-        joinWindows,
-        sendBeacon,
-        preopenJoinWindow,
-        navigateJoinWindow
+        sendBeacon
     };
 })();
