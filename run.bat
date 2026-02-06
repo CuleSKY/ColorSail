9fe5c9814127c8d8cb014de923db187f956a294d
Asuka Langley <liwenyu2021@gmail.com>
2026-02-05 18:23:07 -0700

    精简

--- run.bat
+++ run.bat
@@ -1,5 +1,9 @@
 set APP_SECRET_KEY = "Ss1Ktt7G8c5zusQirc6Ah6dgbuIy3WN4c9DEOMnW5L0="
 set AGENT_SHARED_TOKEN =" 2004615abc"
 set SESSION_TTL_DAYS = "30"
-
+set MYSQL_HOST = “152.53.133.165"
+set MYSQL_PORT = 3306
+set MYSQL_DB = "cs2ze_maplist_test_database"
+set MYSQL_USER =  = "cs2ze_maplist_test_database"
+set MYSQL_PASSWORD = "DaHQ83pnDd7k6Fp8"
 python app.py
